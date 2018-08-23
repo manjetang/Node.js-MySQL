0782bb6377fcdb02b07f68004dd0d7c54e75c575
Lionel Manje <manjetang@yahoo.com>
2018-08-23 11:38:34 -0700

    first commit

--- bamazon.sql
+++ bamazon.sql
@@ -11,15 +11,15 @@ PRIMARY KEY(ITEM_ID)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("initiatives nonstick cookware", "kitchen and dining", 54.82, 1),
-("jade matcha green tea", "food and beverage", 9.95, 1),
-("umbuzo wood table", "kitchen and dining", 499, 1),
-("apple macbook pro 13", "electronics", 1037.49, 1),
-("multivitamins", "supplement", 18.95, 1),
-("playstation 4", "electronic and gaming", 299.99, 1),
-("high yield toner cartridge", "school and office supplies", 53.99, 1),
-("dinnerware set", "kitchen and dining", 45.00, 1),
-("brother printer", "office and school supplies", 59.99, 1),
-("call of duty", "software", 59.99, 1);
+VALUES ("initiatives nonstick cookware", "kitchen and dining", 54.82, 10),
+("jade matcha green tea", "food and beverage", 9.95, 10),
+("umbuzo wood table", "kitchen and dining", 499, 10),
+("apple macbook pro 13", "electronics", 1037.49, 10),
+("multivitamins", "supplement", 18.95, 10),
+("playstation 4", "electronic and gaming", 299.99, 10),
+("high yield toner cartridge", "school and office supplies", 53.99, 10),
+("dinnerware set", "kitchen and dining", 45.00, 10),
+("brother printer", "office and school supplies", 59.99, 10),
+("call of duty", "software", 59.99, 10);
 
 SELECT * FROM products;
